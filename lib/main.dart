e763b160e4d252b7edf3cb49b66b5008e0351905
DoZeroJeong <dogug2865@gmail.com>
2020-01-15 23:25:18 +0900

    add todoey_flutter
    Udemy Study

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+import 'package:todoey_flutter/screens/task_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
+    );
+  }
+}
